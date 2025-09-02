6690b1ec6c7f332716149bff9f9c431077ff134a
tapati <tapatikol07@gmail.com>
2025-09-03 00:22:15 +0530

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
     console.log("Forwarding body to GAS:", forwardBody);
     
     // Forward request to GAS
-    const response = await fetch("https://script.google.com/macros/s/AKfycby4lrkai6y5vf9tTHjeQWKSGfNPMRi-j1DQeN6coYno783qUDyf5xINEzPvn3VDs07fMA/exec", {
+    const response = await fetch("https://script.google.com/macros/s/AKfycbxrZIQCyFoZxZVm926CUGpTYj-2uRdkfCX57zhSBZyHC6D_LRBlhh_ItJGjX3bIdYPxJA/exec", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(forwardBody),
